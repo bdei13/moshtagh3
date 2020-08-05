1268f1d806b1f6e687bc52f272332e9f52c76667
bdei13 <bdei6044@gmail.com>
2020-08-05 18:32:43 +0430

    Updates

---
+++ precache-manifest.d0b896551ca0d27930289c7229b4c94f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1b4f089704f8aa6241a99f385035a1",
+    "url": "/moshtagh3/index.html"
+  },
+  {
+    "revision": "b3c96304ab3cd3905ee7",
+    "url": "/moshtagh3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/moshtagh3/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moshtagh3/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c96304ab3cd3905ee7",
+    "url": "/moshtagh3/static/js/main.6d931d32.chunk.js"
+  },
+  {
+    "revision": "e86f535ff61ccfaab48c",
+    "url": "/moshtagh3/static/js/runtime-main.804a19a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moshtagh3/static/media/logo.5d5d9eef.svg"
+  }
+]);
